1872689c35b380754fda4b8b7bd814a8cfcee523
Ted Lin <ted.lin@infostrum.com>
2022-06-23 15:15:35 +0800

    update Kth Larget Element in an Array

--- leetcode/Kth Largest Element in an Array.go
+++ leetcode/Kth Largest Element in an Array.go
@@ -6,24 +6,37 @@ import (
 )
 
 func findKthLargest(nums []int, k int) int {
-
+	return kthLargest(nums, len(nums)-k, 0, len(nums)-1)
 }
 
-func kthLargest(nums []int, targeIdx int, left int, right int) int {
+func kthLargest(nums []int, targetIdx int, left int, right int) int {
 	pivotIdx := rand.Intn(right-left+1) + left
 	pivot := nums[pivotIdx]
 
 	//pivot move to right
 	nums[pivotIdx], nums[right] = nums[right], nums[pivotIdx]
 
+	//lower than pivot move to pivotIdx's left, others move to pivotIdx's right
 	pivotIdx = left
-	for i := left + 1; i < right; i++ {
-		if nums[i] < pivot {
+	for i := left; i < right; i++ {
+		if nums[i] <= pivot {
 			nums[pivotIdx], nums[i] = nums[i], nums[pivotIdx]
 			pivotIdx++
 		}
 	}
-	//TODO
+
+	//pivot move to pivotIdx
+	nums[pivotIdx], nums[right] = nums[right], nums[pivotIdx]
+
+	if pivotIdx == targetIdx {
+		return nums[pivotIdx]
+	} else if pivotIdx < targetIdx {
+		left = pivotIdx + 1
+		return kthLargest(nums, targetIdx, left, right)
+	} else {
+		right = pivotIdx - 1
+		return kthLargest(nums, targetIdx, left, right)
+	}
 }
 
 func TestfindKthLargest() {
