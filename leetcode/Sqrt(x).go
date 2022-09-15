4b14a1d00277f2ea8eeb03c5604ee43491907558
Ted Lin <ted.lin@infostrum.com>
2022-09-15 10:56:54 +0800

    add Sqrt(x) and UTF-8 Validation

---
+++ leetcode/Sqrt(x).go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "fmt"
+
+func mySqrt(x int) int {
+	if x <= 1 {
+		return x
+	}
+	left, right := 1, x/2
+	for left < right {
+		mid := (left + right + 1) / 2
+		if mid*mid > x {
+			right = mid - 1
+		} else {
+			left = mid
+		}
+	}
+
+	return left
+}
+
+func Test_mySqrt() {
+	fmt.Println(mySqrt(0)) //0
+	fmt.Println(mySqrt(2)) //1
+	fmt.Println(mySqrt(4)) //2
+	fmt.Println(mySqrt(8)) //2
+}
