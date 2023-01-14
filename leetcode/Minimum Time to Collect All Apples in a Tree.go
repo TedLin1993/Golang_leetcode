1384e7b26f5fcb921838f2d49bc9bac38e3f4276
TedLin1993 <xup6y3ru04@gmail.com>
2023-01-14 16:18:06 +0800

    update Minimum Time to Collect All Apples in a Tree

--- leetcode/Minimum Time to Collect All Apples in a Tree.go
+++ leetcode/Minimum Time to Collect All Apples in a Tree.go
@@ -3,34 +3,42 @@ package leetcode
 import "fmt"
 
 func minTime(n int, edges [][]int, hasApple []bool) int {
-	connections := make(map[int][]int, len(edges))
+	connections := make([][]int, n)
+	for i := 0; i < len(connections); i++ {
+		connections[i] = make([]int, 0, 3)
+	}
+	visited := make([]bool, n)
+	visited[0] = true
 	for _, edge := range edges {
 		connections[edge[0]] = append(connections[edge[0]], edge[1])
+		connections[edge[1]] = append(connections[edge[1]], edge[0])
 	}
 
 	sum := 0
 	if connections[0] != nil {
 		for _, v := range connections[0] {
-			sum += dfs_minTime(v, connections, hasApple)
+			sum += dfs_minTime(v, connections, hasApple, visited)
 		}
 	}
 	return sum
 }
 
-func dfs_minTime(vertex int, connections map[int][]int, hasApple []bool) int {
+func dfs_minTime(vertex int, connections [][]int, hasApple []bool, visited []bool) int {
 	sum := 0
-	if connections[vertex] != nil {
-		for _, v := range connections[vertex] {
-			sum += dfs_minTime(v, connections, hasApple)
+	visited[vertex] = true
+	for _, v := range connections[vertex] {
+		if !visited[v] {
+			sum += dfs_minTime(v, connections, hasApple, visited)
 		}
 	}
 
 	if hasApple[vertex] || sum > 0 {
-		return sum + 2
+		sum += 2
 	}
-	return 0
+	return sum
 }
 
 func Test_minTime() {
 	fmt.Println(minTime(7, [][]int{{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6}}, []bool{false, false, true, false, true, true, false})) //8
+	fmt.Println(minTime(4, [][]int{{0, 2}, {0, 3}, {1, 2}}, []bool{false, true, false, false}))                                            //4
 }
