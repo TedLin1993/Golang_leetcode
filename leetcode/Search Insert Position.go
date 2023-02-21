7c58dcbff716b92e43dd59584867b1313af41236
ted <xup6y3ru04@gmail.com>
2023-02-21 16:37:25 +0800

    add Search Insert Position

---
+++ leetcode/Search Insert Position.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func searchInsert(nums []int, target int) int {
+	if target < nums[0] {
+		return 0
+	}
+	if target > nums[len(nums)-1] {
+		return len(nums)
+	}
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return left
+}
