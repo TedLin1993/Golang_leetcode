1b40971460e30d3bfd5f8fcfa742f3c0acc513bb
ted <xup6y3ru04@gmail.com>
2023-03-23 14:28:03 +0800

    add The Number of Beautiful Subsets

--- leetcode/The Number of Beautiful Subsets.go
+++ leetcode/The Number of Beautiful Subsets.go
@@ -3,45 +3,30 @@ package leetcode
 import "fmt"
 
 func beautifulSubsets(nums []int, k int) int {
-	sets := [][]int{}
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			diff := nums[j] - nums[i]
-			if diff < 0 {
-				diff = -diff
-			}
-			if diff != k {
-				sets = append(sets, []int{i, j})
-			}
+	var dfs func(idx int, containsMap map[int]int) int
+	dfs = func(idx int, containsMap map[int]int) int {
+		if containsMap[nums[idx]+k] > 0 || containsMap[nums[idx]-k] > 0 {
+			return 0
 		}
+		res := 1
+		containsMap[nums[idx]]++
+		for i := idx + 1; i < len(nums); i++ {
+			res += dfs(i, containsMap)
+		}
+		containsMap[nums[idx]]--
+		return res
 	}
-
-	res := len(nums) + len(sets)
-	// for len(sets) > 1 {
-	// 	nextSet := make([][]int, 0, len(sets))
-	// 	//TODO
-	// 	for i := 0; i < len(sets); i++ {
-	// 		for j := i + 1; j < len(sets); j++ {
-	// 			set1 := sets[i]
-	// 			set2 := sets[j]
-	// 			count := 0
-	// 			for k := 0; k< len(sets[j]); k++ {
-	// 				if
-	// 			}
-	// 		}
-	// 	}
-
-	// 	sets = nextSet
-	// }
-
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		containsMap := make(map[int]int, len(nums)-i)
+		res += dfs(i, containsMap)
+	}
 	return res
 }
 
-// func canMerge([][]int) (bool, []int) {
-
-// }
-
 func Test_beautifulSubsets() {
-	// fmt.Println(beautifulSubsets([]int{2, 4, 6}, 2)) //4
-	fmt.Println(beautifulSubsets([]int{2, 4, 6, 7}, 2)) //9
+	fmt.Println(beautifulSubsets([]int{2, 4, 6}, 2))           //4
+	fmt.Println(beautifulSubsets([]int{2, 4, 6, 7}, 2))        //9
+	fmt.Println(beautifulSubsets([]int{4, 2, 5, 9, 10, 3}, 1)) //23
+	fmt.Println(beautifulSubsets([]int{1, 1, 2, 3}, 1))        //8
 }
