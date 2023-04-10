34cc719e27144708e5fb9b41614d7222393c936c
ted <xup6y3ru04@gmail.com>
2023-04-10 10:07:41 +0800

    update Valid Parentheses

--- leetcode/Valid Parentheses.go
+++ leetcode/Valid Parentheses.go
@@ -27,6 +27,21 @@ func checkBucket(rightBucket byte, leftBucket byte) bool {
 	return false
 }
 
+func isValid2(s string) bool {
+	stack := make([]byte, 0, len(s)/2)
+	bracketMap := map[byte]byte{')': '(', '}': '{', ']': '['}
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '{' || s[i] == '[' {
+			stack = append(stack, s[i])
+		} else if len(stack) == 0 || bracketMap[s[i]] != stack[len(stack)-1] {
+			return false
+		} else {
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
+
 func TestisValid() {
 	fmt.Println(isValid("()"))     //true
 	fmt.Println(isValid("()[]{}")) //true
