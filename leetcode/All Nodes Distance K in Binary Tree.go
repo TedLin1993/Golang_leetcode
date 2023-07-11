b5b01b324f9080de73307ed688a7933218f29c2e
TedLin1993 <xup6y3ru04@gmail.com>
2023-07-11 15:29:11 +0800

    add All Nodes Distance K in Binary Tree

---
+++ leetcode/All Nodes Distance K in Binary Tree.go
@@ -0,0 +1,46 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func distanceK(root *TreeNode, target *TreeNode, k int) []int {
+	max := 500
+	nodeMap := make([][]int, max+1)
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		node := queue[0]
+		if node.Left != nil {
+			nodeMap[node.Val] = append(nodeMap[node.Val], node.Left.Val)
+			nodeMap[node.Left.Val] = append(nodeMap[node.Left.Val], node.Val)
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			nodeMap[node.Val] = append(nodeMap[node.Val], node.Right.Val)
+			nodeMap[node.Right.Val] = append(nodeMap[node.Right.Val], node.Val)
+			queue = append(queue, node.Right)
+		}
+		queue = queue[1:]
+	}
+
+	queueVal := []int{target.Val}
+	visited := make([]bool, max+1)
+	for k > 0 {
+		length := len(queueVal)
+		for i := 0; i < length; i++ {
+			visited[queueVal[i]] = true
+			for _, val := range nodeMap[queueVal[i]] {
+				if !visited[val] {
+					queueVal = append(queueVal, val)
+				}
+			}
+		}
+		queueVal = queueVal[length:]
+		k--
+	}
+	return queueVal
+}
