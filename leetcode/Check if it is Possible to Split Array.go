eba5bfe53726334dcbfa5c217a1c8131ced7df1f
TedLin1993 <xup6y3ru04@gmail.com>
2023-08-07 14:59:14 +0800

    add Check if it is Possible to Split Array

--- leetcode/Check if it is Possible to Split Array.go
+++ leetcode/Check if it is Possible to Split Array.go
@@ -1,2 +1,28 @@
 package leetcode
 
+func canSplitArray(nums []int, m int) bool {
+	n := len(nums)
+	if n <= 2 {
+		return true
+	}
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+	for i := 0; i < n-1; i++ {
+		if nums[i]+nums[i+1] >= m {
+			dp[i][i+1] = true
+		} else {
+			dp[i][i+1] = false
+		}
+	}
+	for gap := 2; gap < n; gap++ {
+		for left := 0; left < n-gap; left++ {
+			right := left + gap
+			if dp[left][right-1] || dp[left+1][right] {
+				dp[left][right] = true
+			}
+		}
+	}
+	return dp[0][n-1]
+}
