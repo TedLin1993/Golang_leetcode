70b6c8cb158ec9af9c778070344320d24db5a103
TedLin1993 <xup6y3ru04@gmail.com>
2023-08-25 11:05:10 +0800

    update Interleaving String

--- leetcode/Interleaving String.go
+++ leetcode/Interleaving String.go
@@ -3,26 +3,19 @@ package leetcode
 import "fmt"
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	if len(s3) != len(s1)+len(s2) {
+	n1, n2, n3 := len(s1), len(s2), len(s3)
+	if n3 != n1+n2 {
 		return false
 	}
-
-	dp := make([]bool, len(s2)+1)
-	for i := 0; i <= len(s1); i++ {
-		for j := 0; j <= len(s2); j++ {
-			if i == 0 && j == 0 {
-				dp[0] = true
-			} else if i == 0 {
-				dp[j] = dp[j-1] && s2[j-1] == s3[i+j-1]
-			} else if j == 0 {
-				dp[j] = dp[j] && s1[i-1] == s3[i+j-1]
-			} else {
-				dp[j] = (dp[j-1] && s2[j-1] == s3[i+j-1]) || (dp[j] && s1[i-1] == s3[i+j-1])
-			}
+	dp := make([]bool, n2+1)
+	for i := 0; i <= n1; i++ {
+		for j := 0; j <= n2; j++ {
+			dp[j] = (i == 0 && j == 0) ||
+				(i > 0 && dp[j] && s1[i-1] == s3[i+j-1]) ||
+				(j > 0 && dp[j-1] && s2[j-1] == s3[i+j-1])
 		}
 	}
-
-	return dp[len(s2)]
+	return dp[n2]
 }
 
 func TestisInterleave() {
