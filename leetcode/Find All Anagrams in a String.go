cff7973ca97099a9982a3189fb714a0dfc9717bf
TedLin1993 <xup6y3ru04@gmail.com>
2023-10-24 11:16:04 +0800

    update

--- leetcode/Find All Anagrams in a String.go
+++ leetcode/Find All Anagrams in a String.go
@@ -3,43 +3,22 @@ package leetcode
 import "fmt"
 
 func findAnagrams(s string, p string) []int {
-	if len(s) < len(p) {
-		return []int{}
+	sLen, pLen := len(s), len(p)
+	sCount, pCount := [26]int{}, [26]int{}
+	for i := 0; i < pLen; i++ {
+		pCount[p[i]-'a']++
 	}
-
-	pArray := make([]int, 26) //a-z 26 characters
-	for i := 0; i < len(p); i++ {
-		pArray[p[i]-'a']++
-	}
-
-	currentArray := make([]int, 26)
-	//first len(p) char
-	for i := 0; i < len(p); i++ {
-		currentArray[s[i]-'a']++
-	}
-
 	res := []int{}
-	for i := len(p); i < len(s); i++ {
-		if checkAnagram(pArray, currentArray) {
-			res = append(res, i-len(p))
+	for i := 0; i < sLen; i++ {
+		sCount[s[i]-'a']++
+		if i >= pLen {
+			sCount[s[i-pLen]-'a']--
 		}
-		currentArray[s[i-len(p)]-'a']--
-		currentArray[s[i]-'a']++
+		if pCount == sCount {
+			res = append(res, i-pLen+1)
+		}
 	}
-	if checkAnagram(pArray, currentArray) {
-		res = append(res, len(s)-len(p))
-	}
-
 	return res
-}
-
-func checkAnagram(pArray []int, currentArray []int) bool {
-	for i := 0; i < 26; i++ {
-		if pArray[i] != currentArray[i] {
-			return false
-		}
-	}
-	return true
 }
 
 func TestFindAnagrams() {
