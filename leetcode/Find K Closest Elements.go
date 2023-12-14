6e2a38ee5fe1da666a2af984c5dc8da89c1d6313
TedLin1993 <xup6y3ru04@gmail.com>
2023-12-14 16:08:57 +0800

    add Find K Closest Elements

---
+++ leetcode/Find K Closest Elements.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func findClosestElements(arr []int, k int, x int) []int {
+	if x <= arr[0] {
+		return arr[:k]
+	}
+	left, right := 0, len(arr)-k
+	for left < right {
+		mid := left + (right-left)/2
+		if x-arr[mid] > arr[mid+k]-x {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return arr[left : left+k]
+}
