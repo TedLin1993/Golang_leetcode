d945f8c32abce7441947eb3602b56dbe885c3cc6
TedLin1993 <xup6y3ru04@gmail.com>
2024-01-09 10:14:42 +0800

    update

--- leetcode/ReverseInteger.go
+++ leetcode/ReverseInteger.go
@@ -24,6 +24,18 @@ func reverse(x int) int {
 	return res
 }
 
+func reverse_2(x int) int {
+	res := 0
+	for x != 0 {
+		res = res*10 + x%10
+		if res > math.MaxInt32 || res < math.MinInt32 {
+			return 0
+		}
+		x /= 10
+	}
+	return res
+}
+
 func Test_reverse() {
 	var x int
 	x = 123
