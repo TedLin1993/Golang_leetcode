1f00761c7542c804e7350b853abd6cdb9db31d0f
TedLin1993 <xup6y3ru04@gmail.com>
2024-01-23 11:41:08 +0800

    update

--- leetcode/Maximum Length of a Concatenated String with Unique Characters.go
+++ leetcode/Maximum Length of a Concatenated String with Unique Characters.go
@@ -27,6 +27,30 @@ func dfs_maxLength(arr []string, index int, uniqChars [26]bool) int {
 	return res + len(str)
 }
 
+func maxLength_2(arr []string) int {
+	n := len(arr)
+	res := 0
+	var dfs func(cache [26]bool, idx int, curLen int)
+	dfs = func(cache [26]bool, idx int, curLen int) {
+		a := arr[idx]
+		for i := 0; i < len(a); i++ {
+			if cache[a[i]-'a'] {
+				return
+			}
+			cache[a[i]-'a'] = true
+		}
+		curLen += len(a)
+		res = max(res, curLen)
+		for i := idx + 1; i < n; i++ {
+			dfs(cache, i, curLen)
+		}
+	}
+	for i := 0; i < n; i++ {
+		dfs([26]bool{}, i, 0)
+	}
+	return res
+}
+
 func Test_maxLength() {
 	fmt.Println(maxLength([]string{"un", "iq", "ue"}))                                                               //4
 	fmt.Println(maxLength([]string{"r", "cha", "act", "ers"}))                                                       //6
