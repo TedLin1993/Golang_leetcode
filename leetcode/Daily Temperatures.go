c8374af8c71ec1b6b28ac15556e6d28539d9fabe
TedLin1993 <xup6y3ru04@gmail.com>
2024-01-31 11:23:30 +0800

    update

--- leetcode/Daily Temperatures.go
+++ leetcode/Daily Temperatures.go
@@ -15,3 +15,23 @@ func dailyTemperatures(t []int) []int {
 	}
 	return res
 }
+
+func dailyTemperatures_2(temperatures []int) []int {
+	n := len(temperatures)
+	type pair struct {
+		t   int
+		idx int
+	}
+	stack := make([]pair, 0, n)
+	res := make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		for len(stack) > 0 && temperatures[i] >= stack[len(stack)-1].t {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) > 0 {
+			res[i] = stack[len(stack)-1].idx - i
+		}
+		stack = append(stack, pair{t: temperatures[i], idx: i})
+	}
+	return res
+}
