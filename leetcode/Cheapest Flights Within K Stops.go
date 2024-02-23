5bd3d2fddc9bef8c2fa8441b08f8b9d77ce2b1d6
TedLin1993 <xup6y3ru04@gmail.com>
2024-02-23 21:17:15 +0800

    update

--- leetcode/Cheapest Flights Within K Stops.go
+++ leetcode/Cheapest Flights Within K Stops.go
@@ -58,3 +58,41 @@ func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
 	}
 	return -1
 }
+
+func findCheapestPrice_2(n int, flights [][]int, src int, dst int, k int) int {
+	type fly struct {
+		to    int
+		price int
+	}
+	graph := make(map[int][]fly)
+	for _, f := range flights {
+		graph[f[0]] = append(graph[f[0]], fly{f[1], f[2]})
+	}
+	costs := make([]int, n)
+	for i := 0; i < n; i++ {
+		costs[i] = math.MaxInt
+	}
+	costs[src] = 0
+	queue := []int{src}
+	for len(queue) > 0 && k >= 0 {
+		temp := make([]int, n)
+		copy(temp, costs)
+		count := len(queue)
+		for i := 0; i < count; i++ {
+			city := queue[i]
+			for _, f := range graph[city] {
+				if costs[city]+f.price < temp[f.to] {
+					temp[f.to] = costs[city] + f.price
+					queue = append(queue, f.to)
+				}
+			}
+		}
+		costs = temp
+		queue = queue[count:]
+		k--
+	}
+	if costs[dst] == math.MaxInt {
+		return -1
+	}
+	return costs[dst]
+}
