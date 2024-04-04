cb2de20c3fb4edd2117164af0af17c73d5a68f9c
TedLin1993 <xup6y3ru04@gmail.com>
2024-04-04 18:31:09 +0800

    add Maximum Nesting Depth of the Parentheses

---
+++ leetcode/Maximum Nesting Depth of the Parentheses.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func maxDepth(s string) int {
+    res := 0
+    cur := 0
+    for _, c := range s {
+        if c == '(' {
+            cur++
+            res = max(res, cur)
+        } else if c == ')' {
+            cur--
+        }
+    }
+    return res
+}
